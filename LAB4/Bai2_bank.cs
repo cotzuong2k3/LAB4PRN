62885c2e9edf103cc4a72ac4db9ddfdf49a3384b
cotzuong2k3 <114601947+cotzuong2k3@users.noreply.github.com>
2023-09-28 17:26:33 +0700

    Add project files.

---
+++ LAB4/Bai2_bank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LAB4
+{
+    internal class Bai2_bank
+    {
+        public delegate void BalanceChangedEventHandler(decimal newBalance);
+
+
+        public class Account
+        {
+            public event BalanceChangedEventHandler BalanceChanged;
+
+            private decimal balance;
+
+            public decimal Balance
+            {
+                get { return balance; }
+                set
+                {
+                    balance = value;
+                    OnBalanceChanged(balance);
+                }
+            }
+
+            protected virtual void OnBalanceChanged(decimal newBalance)
+            {
+                BalanceChanged?.Invoke(newBalance);
+            }
+
+        }
+
+        public class Program
+        {
+            static void Main()
+            {
+                Account account = new Account();
+                account.BalanceChanged += HandleBalanceChanged;
+
+
+                decimal transactionAmount = 100.00M;
+                account.Balance += transactionAmount;
+
+                transactionAmount = 200.00M;
+                account.Balance += transactionAmount;
+            }
+
+            static void HandleBalanceChanged(decimal newBalance)
+            {
+                Console.WriteLine($"New account Balance: {newBalance}");
+            }
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
---
+++ LAB4/SampleDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB4
+{
+    internal class SampleDelegate
+    {
+        public delegate void Delegate(int a, int b);
+        class MathOperations
+        {
+            public void Add(int a, int b)
+            {
+                Console.WriteLine("Add result: {0}", a + b);
+            }
+
+            public void Sub(int a, int b)
+            {
+                Console.WriteLine("Sub result: {0}", a - b);
+            }
+
+            public void Mul(int a, int b)
+            {
+                Console.WriteLine("Mul result: {0}", a * b);
+            }
+
+            static void Main(String[] args)
+            {
+                Console.WriteLine("---Delegete Example---");
+                MathOperations m = new MathOperations();
+                Delegate dlgt = m.Add;
+
+                dlgt += m.Sub;
+                dlgt += m.Mul;
+
+                dlgt(10, 90);
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
